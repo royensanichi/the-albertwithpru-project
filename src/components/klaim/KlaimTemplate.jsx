ca717860239c24a67a4cf39796bc41c5dcdb8481
RoyenSanichi <ravexroyen@gmail.com>
2025-09-02 16:16:37 +0700

    update on Home, will run on Production

--- src/components/klaim/KlaimTemplate.jsx
+++ src/components/klaim/KlaimTemplate.jsx
@@ -2,7 +2,7 @@ const KlaimTemplate = ({ klaim }) => {
   return (
     <div className="flex flex-col h-full">
       <div className="text-center mx-auto">
-        <p className="text-xs sm:text-[14px] md:text-[18px] pb-5 sm:pb-10 md:pb-14 max-w-144.25 text-soft-dark">
+        <p className="text-xs sm:text-[14px] md:text-[18px] pb-5 sm:pb-10 md:pb-14 max-w-144.25 text-dark">
           {klaim?.message}
         </p>
       </div>
