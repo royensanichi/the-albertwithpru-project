1bdee4bb271cbbfd3067b2bca16809a0c37639fe
RoyenSanichi <ravexroyen@gmail.com>
2025-09-03 19:22:29 +0700

    added animation on intro

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -2,6 +2,8 @@ import siloam from "../../assets/images/prusiloam.jpg";
 import group from "../../assets/images/group_photo.jpg";
 import "./introduction.css";
 import InformationSummary from "./InformationSummary";
+import { LazyMotion, domAnimation } from "motion/react";
+import * as m from "motion/react-m";
 
 // Information summary data
 const informationSummaryData = [
@@ -24,57 +26,89 @@ const informationSummaryData = [
 
 const Introduction = () => {
   return (
-    <div
-      className="flex max-lg:flex-col-reverse sm:justify-between lg:items-center lg:mb-27.5 max-xl:gap-2 p-2 max-xxl:px-4"
-      id="introduction"
-    >
-      <img  src={ window.innerWidth > 1024 ? group : siloam}
-          alt="group" 
-             className="absolute inset-0 w-full max-h-350 object-cover opacity-60 -z-0 blur-xs"
-      />
-      <div className="w-full flex flex-col justify-between max-lg:text-center z-10 ">
-        <div className="pt-4 me-31.5 w-full xl:w-auto transition-all duration-500 text-white">
-          <p className="text-sm sm:text-base w-full lg:w-auto text-center lg:text-start mb-3 drop-shadow-[0_0_4px_black]">
-            <span className="font-semibold text-3xl sm:max-xl:text-5xl xl:text-7xl text-nowrap shrink-0 inline-block w-full">Agen Terbaik Anda</span>
-            {/* <span className="font-semibold text-2xl sm:max-xl:text-5xl xl:text-6xl text-nowrap shrink-0 inline-block w-full">#AlbertWithPru</span> */}
-            <span className="drop-shadow-[0_0_2px_black]">
-            Albert Sany | Servicing Agent Prudential
-             </span>
-          </p>
-          <p className="text-xs xxs:text-lg lg:text-xl my-6 bg-white rounded-xl text-black font-semibold p-4 opacity-90">
-           "Siap membantu Anda merencanakan keuangan dan melindungi masa depan keluarga Anda."
-          </p>
-          <p className="text-center lg:text-start">
-            <a
-              className="btn-primary btn btn-xs xxs:btn-lg text-white"
-              href="https://wa.me/6287883916216?text=Halo Albert, Saya (Nama Anda) ingin berkonsultansi dengan Anda!"
+    <LazyMotion features={domAnimation}>
+      <m.div
+        className="flex max-lg:flex-col-reverse sm:justify-between lg:items-center lg:mb-27.5 max-xl:gap-2 p-2 max-xxl:px-4"
+        id="introduction"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.8 }}
+      >
+        <img
+          src={window.innerWidth > 1024 ? group : siloam}
+          alt="group"
+          className="absolute inset-0 w-full max-h-350 object-cover opacity-60 -z-0 blur-xs"
+        />
+        <m.div className="w-full flex flex-col justify-between max-lg:text-center z-10 ">
+          <div className="pt-4 me-31.5 w-full xl:w-auto transition-all duration-500 text-white">
+            <p className="text-sm sm:text-base w-full lg:w-auto text-center lg:text-start mb-3 drop-shadow-[0_0_4px_black]">
+              <m.span
+                className="font-semibold text-3xl sm:max-xl:text-5xl xl:text-7xl text-nowrap shrink-0 inline-block w-full"
+                initial={{ y: -20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.8, delay: 0.3 }}
+              >
+                Agen Terbaik Anda
+              </m.span>
+              {/* <span className="font-semibold text-2xl sm:max-xl:text-5xl xl:text-6xl text-nowrap shrink-0 inline-block w-full">#AlbertWithPru</span> */}
+              <m.span
+                className="drop-shadow-[0_0_2px_black]"
+                //  initial={{ y: -80, opacity:0 }}
+                animate={{ y: [-80, 0], opacity: [0, 1] }}
+                transition={{ duration: 0.8, delay: 1.3 }}
+              >
+                Albert Sany | Servicing Agent Prudential
+              </m.span>
+            </p>
+            <m.p
+              className="text-xs xxs:text-lg lg:text-xl my-6 bg-white rounded-xl text-black font-semibold p-4 opacity-90"
+              animate={{ y: [-30, 0], opacity: [0, 1] }}
+              transition={{ duration: 0.8, delay: 2.3 }}
             >
-              Konsultasi Gratis!
-            </a>
-          </p>
-        </div>
+              "Siap membantu Anda merencanakan keuangan dan melindungi masa
+              depan keluarga Anda."
+            </m.p>
+            <p className="text-center lg:text-start">
+              <m.a
+                className="btn-primary btn btn-xs xxs:btn-lg text-white"
+                href="https://wa.me/6287883916216?text=Halo Albert, Saya (Nama Anda) ingin berkonsultansi dengan Anda!"
+                animate={{ y: [-30, 0], opacity: [0, 1] }}
+                transition={{ duration: 0.8, delay: 2.3 }}
+                whileHover={{
+                  scale: 1.1,
+                  transition: { duration: 0 },
+                }}
+              >
+                Konsultasi Gratis!
+              </m.a>
+            </p>
+          </div>
 
-        {/* Informasi Ringkas */}
-        {/* <div className="mx-auto lg:mx-0 relative">
+          {/* Informasi Ringkas */}
+          {/* <div className="mx-auto lg:mx-0 relative">
           <div className="grid max-xxs:grid-flow-col grid-cols-3 w-fit mt-10 gap-1">
             {informationSummaryData.map((item) => (
               <InformationSummary key={item.id} item={item} />
             ))}
           </div>
         </div> */}
-      </div>
-      
-      <div
-        className={`max-w-128 w-full h-auto max-lg:mx-auto aspect-[536/636] relative`}
-      >
-        
-        <img
-          className={window.innerWidth > 1024 ? `shadow-2xl shadow-gray-200 w-full h-auto absolute inset-0 m-auto object-contain bg-white rounded-3xl sm:`: `shadow-2xl shadow-gray-200 w-full h-auto absolute bottom-auto object-contain bg-white rounded-3xl`}
-          src={ window.innerWidth > 1024 ? group : siloam}
-          alt="group"
-        />
-      </div>
-    </div>
+        </m.div>
+
+        <m.div
+          className={`max-w-128 w-full h-auto max-lg:mx-auto aspect-[536/636] relative`}
+        >
+          <img
+            className={
+              window.innerWidth > 1024
+                ? `shadow-2xl shadow-gray-200 w-full h-auto absolute inset-0 m-auto object-contain bg-white rounded-3xl sm:`
+                : `shadow-2xl shadow-gray-200 w-full h-auto absolute bottom-auto object-contain bg-white rounded-3xl`
+            }
+            src={window.innerWidth > 1024 ? group : siloam}
+            alt="group"
+          />
+        </m.div>
+      </m.div>
+    </LazyMotion>
   );
 };
 
