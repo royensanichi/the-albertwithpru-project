4b845b2823af461a0d52ff890be0ddd6187b100a
RoyenSanichi <ravexroyen@gmail.com>
2025-09-24 00:23:20 +0700

    remove domisili and email,making a simple form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -34,29 +34,30 @@ const Contact = () => {
       >
         <div className="flex flex-col-reverse lg:gap-5 xl:gap-25.75 lg:flex-row justify-between">
           <div>
-            <div>
+            {/* <div> */}
               <p className="text-[35px] max-lg:hidden font-semibold text-nowrap text-[#132238]">
                 Konsultasikan Rencana Anda!
               </p>
               <p className="text-[12px] xs:text-[14px] sm:text-lg md:text-lg max-lg:text-center pt-4 font-normal text-soft-dark">
                 Saya selalu open dan siap membantu anda, tidak dipungut biaya ya.
               </p>
-            </div>
-            <div className="my-8.75 sm:max-lg:flex justify-between items-center">
+            {/* </div> */}
+            {/* <div className="my-8.75 sm:max-lg:flex justify-between items-center">
               {addressData.map((item, index) => (
                 <Address item={item} key={index} />
               ))}
             </div>
             <div className="w-full max-lg:text-center max-md:mb-4">
               <SocialMedia />
-            </div>
-          </div>
+            </div> */}
+         
           <div className="w-full overflow-y-scroll py-6.5">
             <p className="text-xl mb-2 xs:text-2xl sm:text-2xl md:text-[38px] font-semibold text-[#132238] lg:hidden text-center">
               Yuk Konsultasikan Rencana Keuangan Anda!
             </p>
             <Form />
           </div>
+           </div>
         </div>
       </div>
     </div>
